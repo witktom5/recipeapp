15ee01528402325d7053043de0ddf8b09473217e
witktom5 <witktom5@gmail.com>
2023-09-17 17:48:59 +0200

    style body, update schema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main>
         <div>{recipes?.map((recipe) => `${recipe.title} `)}</div>
       </main>
     </>
