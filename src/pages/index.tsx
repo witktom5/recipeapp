2a13764f6e6973bff745e5af16664d6efae8b0c1
witktom5 <witktom5@gmail.com>
2023-09-17 18:28:28 +0200

    update readme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <title>Recipe App</title>
         <meta
           name="description"
-          content="App for looking up new cooking recipes"
+          content="App to help you find new cooking recipes"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
